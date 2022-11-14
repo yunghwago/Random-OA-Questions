557ed7e82fa85570dee7ceb312325bd0b704fceb
Christopher Go <45017839+yunghwago@users.noreply.github.com>
2022-11-14 12:50:16 -0800

    Create Stack - Min Stack.py

---
+++ Stack - Min Stack.py
@@ -0,0 +1,31 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.minStack:
+            self.minStack.append(val)
+        else:
+            self.minStack.append(min(val,self.minStack[-1]))
+    def pop(self) -> None:
+        val = self.stack.pop()
+        self.minStack.pop()
+        return val
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+Console
